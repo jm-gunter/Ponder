ed5fa9020f6dbe4daeaf3c76713b9d787133d6a1
Jeff Gunter <gunter.jeffm@gmail.com>
2019-02-21 21:00:29 -0500

    checkpoint

--- Ponder.Api/Startup.cs
+++ Ponder.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Ponder.Data;
+using Ponder.Models;
 
 namespace Ponder.Api
 {
@@ -26,6 +28,9 @@ namespace Ponder.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register dependencies
+            services.AddScoped<IDataContext<Game>, MongoContext<Game>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
